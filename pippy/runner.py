67e00544491797cb75f04c838e241c1df942ca27
Reuven V. Gonzales <reuven@tobetter.us>
2012-03-28 16:26:17 -0700

    Added basic command line parser for pippy

--- pippy/runner.py
+++ pippy/runner.py
@@ -1,8 +1,9 @@
 import sys
+import argparse
 from pip import version_control
 from pip import req
 from pip.commands import install
-from pip.baseparser import parser
+from pip.baseparser import parser as pip_parser
 from finder import *
 from req import PippyRequirementSet
 
@@ -18,13 +19,19 @@ class ExactInstallCommand(install.InstallCommand):
                              use_mirrors=options.use_mirrors,
                              mirrors=options.mirrors)
 
+main_parser = argparse.ArgumentParser(
+    description='Install pip requirements using a local cache')
+main_parser.add_argument('file', help='a pip requirements file')
+
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
     version_control()
     install = ExactInstallCommand()
-    options, blargs = parser.parse_args([])
-    install.main(['-r', 'requirements.txt'], options)
+    options, pargs = pip_parser.parse_args([])
+    cli_options = main_parser.parse_args(args)
+    requirements_file = cli_options.file
+    install.main(['-r', requirements_file], options)
 
 if __name__ == '__main__':
     main()
